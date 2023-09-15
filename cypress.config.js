f53749910f331a940a74a2b64f775fb8bf882fa7
gabriel <gabrielcmoris@gmail.com>
2023-09-15 15:53:35 +0200

    cypress init

--- cypress.config.js
+++ cypress.config.js
@@ -5,6 +5,6 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    specPattern: "cypress/e2e/**/*.{js,jsx,tsx,feature}",
   },
-  specPattern: "cypress/e2e/**/*.{js,jsx,tsx,feature}",
 });
