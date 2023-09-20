d15b050f78a6addc6e5e8036df68b36dfd6ffecb
gabriel <gabrielcmoris@gmail.com>
2023-09-20 15:50:49 +0200

    improve selectors

--- cypress/e2e/automation-test-store/contact-us.cy.js
+++ cypress/e2e/automation-test-store/contact-us.cy.js
@@ -3,7 +3,13 @@
 
 describe("Test Contact Us form via Automation Test store", () => {
   it("Should be able to submit a succesful submission via Contact Uns Form", () => {
-    cy.visit("https://www.automationteststore.com/")
-
+    cy.visit("https://www.automationteststore.com/");
+    cy.get(
+      "a[href='https://automationteststore.com/index.php?rt=content/contact']"
+    ).click();
+    cy.get("#ContactUsFrm_first_name").type("My first Name");
+    cy.get("#ContactUsFrm_email").type("email@saludos.com");
+    cy.get("#ContactUsFrm_enquiry").type("This is a random message");
+    cy.get("button[title='Submit']").click();
   });
 });
