692b5e6a2b577e10191acf3818220b9a0f51671f
gabriel <gabrielcmoris@gmail.com>
2023-09-22 15:13:49 +0200

    assertions and node commands

--- cypress/e2e/automation-test-store/contact-us.cy.js
+++ cypress/e2e/automation-test-store/contact-us.cy.js
@@ -9,8 +9,13 @@ describe("Test Contact Us form via Automation Test store", () => {
     cy.xpath("//a[contains(@href, 'contact')]").click();
     cy.get("#ContactUsFrm_first_name").type("My first Name");
     cy.get("#ContactUsFrm_email").type("email@saludos.com");
+    cy.get("#ContactUsFrm_email").should("have.attr", "name", "email");
     cy.get("#ContactUsFrm_enquiry").type("This is a random message");
     // This selector is more optimized as .btn > div
     cy.get("button[title='Submit']").click();
+    cy.get(".mb40 > :nth-child(3)").should(
+      "have.text",
+      "Your enquiry has been successfully sent to the store owner!"
+    );
   });
 });
--- cypress/e2e/webdriver-uni/contact-us.cy.js
+++ cypress/e2e/webdriver-uni/contact-us.cy.js
@@ -4,12 +4,24 @@
 describe("Test Contact Us form via WebdriverUni", () => {
   it("Should be able to submit a succesful submission via Contact Uns Form", () => {
     cy.visit("http://www.webdriveruniversity.com/Contact-Us/contactus.html");
+    // To remove an attribute
     // cy.get("#contact-us").invoke("removeAttr", "target").click();
+
+    // If I want to access to the DOM directly:
+    cy.document().should("have.property", "charset").and("eq", "UTF-8");
+
+    // If I want to check the title of the HTML
+    cy.title().should("include", "WebDriver | Contact Us");
+
+    // If I want to access to the URL
+    cy.url().should("include", "contactus.html");
+
     cy.get('[name="first_name"]').type("John");
     cy.get('[name="last_name"]').type("Doe");
     cy.get('[name="email"]').type("johnDoe@yahoo.de");
     cy.get("textarea.feedback-input").type("Lorem Ipsum");
     cy.get('[type="submit"]').click();
+    cy.get("h1").should("have.text", "Thank You for your Message!");
   });
 
   it("Shouldn't be able to submit a succesful submission via Contact Uns Form as all fields are required", () => {
@@ -18,5 +30,6 @@ describe("Test Contact Us form via WebdriverUni", () => {
     cy.get('[name="last_name"]').type("Doe");
     cy.get("textarea.feedback-input").type("Lorem Ipsum");
     cy.get('[type="submit"]').click();
+    cy.get("body").contains("Error: ");
   });
 });
