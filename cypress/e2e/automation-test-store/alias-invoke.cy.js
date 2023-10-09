c0f77b1cb9359fdb2d4c1b87709ee747a14d48db
gabriel <gabrielcmoris@gmail.com>
2023-10-09 15:22:22 +0200

    aliases and invoke

--- cypress/e2e/automation-test-store/alias-invoke.cy.js
+++ cypress/e2e/automation-test-store/alias-invoke.cy.js
@@ -16,7 +16,7 @@ describe("Alias and Invoke", () => {
     cy.get("@productThumbnail").should("include", "Seaweed Conditioner");
   });
 
-  it.only("Challenge: Validate number of products and they contain the title add to cart", () => {
+  it("Challenge: Validate number of products and they contain the title add to cart", () => {
     cy.visit("https://www.automationteststore.com/");
     cy.get(".thumbnail").as("listOfProducts");
     cy.get("@listOfProducts").its("length").should("be.gt", 1);
@@ -24,5 +24,50 @@ describe("Alias and Invoke", () => {
       .find(".productcart")
       .invoke("attr", "title")
       .should("include", "Add to Cart");
+  });
+
+  it("Calculate total of normal and sale products", () => {
+    cy.visit("https://www.automationteststore.com/");
+    cy.get(".thumbnail").as("listOfProducts");
+    // cy.get("@listOfProducts")
+    //   .find(".oneprice")
+    //   .each(($el, index, $list) => {
+    //     $el.text()
+    //   });
+
+    let itemsTotal = 0;
+    cy.get(".thumbnail").find(".oneprice").invoke("text").as("itemPrice");
+    cy.get(".thumbnail").find(".pricenew").invoke("text").as("saleItemPrice");
+
+    //I use the first alias to iterate and add every price in itemsPriceTotal
+
+    cy.get("@itemPrice").then(($linkText) => {
+      cy.log("this will be a concatenated string, not an array", $linkText);
+      let itemsPriceTotal = 0;
+      let itemPrice = $linkText.split("$");
+
+      for (let i = 0; i < itemPrice.length; i++) {
+        itemsPriceTotal += Number(itemPrice[i]);
+      }
+
+      itemsTotal += itemsPriceTotal;
+      cy.log("Non sale price items Total: ", itemsPriceTotal);
+    });
+
+    cy.get("@saleItemPrice")
+      .then(($linkText) => {
+        let saleItemsPriceTotal = 0;
+        let saleItemPrice = $linkText.split("$");
+
+        for (let i = 0; i < saleItemPrice.length; i++) {
+          saleItemsPriceTotal += Number(saleItemPrice[i]);
+        }
+        itemsTotal += saleItemsPriceTotal;
+        cy.log("Sale price items Total: ", saleItemsPriceTotal);
+      })
+      .then(() => {
+        cy.log("The total price of all products: ", itemsTotal);
+        expect(itemsTotal).to.equal(660.5);
+      });
   });
 });
