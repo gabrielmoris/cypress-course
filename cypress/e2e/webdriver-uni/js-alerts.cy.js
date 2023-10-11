7eb19c38f9944217f4702d71dbff0ccc853d2318
gabriel <gabrielcmoris@gmail.com>
2023-10-11 15:50:48 +0200

    stubs and alert handling

--- cypress/e2e/webdriver-uni/js-alerts.cy.js
+++ cypress/e2e/webdriver-uni/js-alerts.cy.js
@@ -10,4 +10,47 @@ describe("Handle js Alerts", () => {
       expect(str).to.equal("I am an alert box!");
     });
   });
+
+  it("Validate JS confirm alert box works correctly when clicking OK", () => {
+    cy.visit("http://www.webdriveruniversity.com/");
+    cy.get("#popup-alerts").invoke("removeAttr", "target").click();
+    cy.get("#button4").click();
+
+    cy.on("window:confirm", (str) => {
+      // click on accept
+      return true;
+    });
+    cy.get("#confirm-alert-text").contains("You pressed OK!");
+  });
+
+  it("Validate JS confirm alert box works correctly when clicking Cancel", () => {
+    cy.visit("http://www.webdriveruniversity.com/");
+    cy.get("#popup-alerts").invoke("removeAttr", "target").click();
+    cy.get("#button4").click();
+
+    cy.on("window:confirm", (str) => {
+      // click on cancel
+      return false;
+    });
+    cy.get("#confirm-alert-text").contains("You pressed Cancel!");
+  });
+
+  it("Validate JS confirm alert box using a stub", () => {
+    cy.visit("http://www.webdriveruniversity.com/");
+    cy.get("#popup-alerts").invoke("removeAttr", "target").click();
+    const stub = cy.stub();
+    cy.on("window:confirm", stub);
+
+    cy.get("#button4")
+      .click()
+      .then(() => {
+        expect(stub.getCall(0)).to.be.calledWith("Press a button!");
+      })
+      .then(() => {
+        return true;
+      })
+      .then(() => {
+        cy.get("#confirm-alert-text").contains("You pressed OK!");
+      });
+  });
 });
