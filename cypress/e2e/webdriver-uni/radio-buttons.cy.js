7e66be4899940041a422d32d085781f16c8ff21c
gabriel <gabrielcmoris@gmail.com>
2023-10-12 09:23:21 +0200

    radio buttons

---
+++ cypress/e2e/webdriver-uni/radio-buttons.cy.js
@@ -0,0 +1,32 @@
+// Putting this on the top of the doccument will allow me to have the right types of Cypress
+/// <reference types="Cypress"/>
+
+describe("Verify radio buttons in webdriveruni", () => {
+  it("Check specific radio buttons", () => {
+    cy.visit("http://www.webdriveruniversity.com/");
+    cy.get("#dropdown-checkboxes-radiobuttons")
+      .invoke("removeAttr", "target")
+      .click({ force: true });
+
+    cy.get("#radio-buttons")
+      .find("input[type='radio']")
+      .as("checkboxes")
+      .first()
+      .check();
+
+    cy.get("@checkboxes").eq(1).check();
+  });
+
+  it("Validate states of specific radio buttons", () => {
+    cy.visit("http://www.webdriveruniversity.com/");
+    cy.get("#dropdown-checkboxes-radiobuttons")
+      .invoke("removeAttr", "target")
+      .click({ force: true });
+
+    cy.get('[value="lettuce"]').should("not.be.checked");
+    cy.get('[value="cabbage"]').should("be.disabled");
+    cy.get('[value="pumpkin"]').should("be.checked");
+    cy.get('[value="lettuce"]').check();
+    cy.get('[value="pumpkin"]').should("not.be.checked");
+  });
+});
