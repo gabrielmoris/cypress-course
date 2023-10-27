74215a1615580fcfc9a240e9201c488c7510bfa9
gabriel <gabrielcmoris@gmail.com>
2023-10-27 14:48:57 +0200

    date picker

--- cypress/e2e/webdriver-uni/datepicker.cy.js
+++ cypress/e2e/webdriver-uni/datepicker.cy.js
@@ -5,12 +5,40 @@ describe("Test datepicker in webdriveruni", () => {
   it("Select date from datepicker", () => {
     cy.visit("http://www.webdriveruniversity.com/");
     cy.get("#datepicker").invoke("removeAttr", "target").click();
-
+    cy.get("#datepicker").click();
     let date = new Date();
-    date.setDate(date.getDate() + 360); // gives the current day
+    date.setDate(date.getDate() + 365); // gives the current day
 
     let futureYear = date.getFullYear();
     let futureMonth = date.toLocaleString("default", { month: "long" });
     let futureDay = date.getDate();
+
+    const selectMonthAndYear = () => {
+      cy.get(".datepicker-dropdown")
+        .find(".datepicker-switch")
+        .eq(0)
+        .then((currentDate) => {
+          if (!currentDate.text().includes(futureYear)) {
+            cy.get(".next").eq(0).click();
+            selectMonthAndYear();
+          }
+        })
+        .then(() => {
+          cy.get(".datepicker-dropdown")
+            .find(".datepicker-switch")
+            .eq(0)
+            .then((currentDate) => {
+              if (!currentDate.text().includes(futureMonth)) {
+                cy.get(".next").eq(0).click();
+                selectMonthAndYear();
+              }
+            });
+        });
+    };
+    const selectFutureDay = () => {
+      cy.get("[class='day']").contains(futureDay).click();
+    };
+    selectMonthAndYear();
+    selectFutureDay();
   });
 });
