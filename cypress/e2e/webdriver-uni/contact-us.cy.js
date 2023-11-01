b78b0cfaff574be30b49135b3a0377b8e4a7ca34
gabriel <gabrielcmoris@gmail.com>
2023-11-01 13:58:51 +0100

    fixtures

--- cypress/e2e/webdriver-uni/contact-us.cy.js
+++ cypress/e2e/webdriver-uni/contact-us.cy.js
@@ -2,6 +2,11 @@
 /// <reference types="Cypress"/>
 
 describe("Test Contact Us form via WebdriverUni", () => {
+  before(() => {
+    cy.fixture("example").then((data) => {
+      globalThis.data = data;
+    });
+  });
   it("Should be able to submit a succesful submission via Contact Uns Form", () => {
     cy.visit("http://www.webdriveruniversity.com/");
     // To remove an attribute
@@ -16,19 +21,19 @@ describe("Test Contact Us form via WebdriverUni", () => {
     // If I want to access to the URL
     cy.url().should("include", "contactus.html");
 
-    cy.get('[name="first_name"]').type("John");
-    cy.get('[name="last_name"]').type("Doe");
-    cy.get('[name="email"]').type("johnDoe@yahoo.de");
-    cy.get("textarea.feedback-input").type("Lorem Ipsum");
+    cy.get('[name="first_name"]').type(data.first_name);
+    cy.get('[name="last_name"]').type(data.last_name);
+    cy.get('[name="email"]').type(data.email);
+    cy.get("textarea.feedback-input").type(data.body);
     cy.get('[type="submit"]').click();
     cy.get("h1").should("have.text", "Thank You for your Message!");
   });
 
   it("Shouldn't be able to submit a succesful submission via Contact Uns Form as all fields are required", () => {
     cy.visit("http://www.webdriveruniversity.com/Contact-Us/contactus.html");
-    cy.get('[name="first_name"]').type("John");
-    cy.get('[name="last_name"]').type("Doe");
-    cy.get("textarea.feedback-input").type("Lorem Ipsum");
+    cy.get('[name="first_name"]').type(data.first_name);
+    cy.get('[name="last_name"]').type(data.last_name);
+    cy.get("textarea.feedback-input").type(data.body);
     cy.get('[type="submit"]').click();
     cy.get("body").contains("Error: ");
   });
