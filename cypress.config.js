d0bf6c1c2dd0416d40a74a5df1df639013d0bb1f
gabriel <gabrielcmoris@gmail.com>
2023-11-03 15:35:28 +0100

    cypress config

--- cypress.config.js
+++ cypress.config.js
@@ -1,13 +1,16 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  projectId: 'mao5un',
+  projectId: "mao5un",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
     specPattern: "cypress/e2e/**/*.{js,jsx,tsx,feature}",
+    exludeSpecPattern: "cypress/e2e/other/*.{js,jsx,tsx,feature}",
     chromeWebSecurity: false,
     experimentalSessionAndOrigin: true,
+    defaultCommandTimeout: 10000,
+    pageLoadTimeout: 120000,
   },
 });
