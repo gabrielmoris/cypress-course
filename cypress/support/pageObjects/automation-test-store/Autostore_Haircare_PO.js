2bbf8d72179e9032d7d1070b2dba3857fbce9e12
gabriel <gabrielcmoris@gmail.com>
2023-11-07 14:53:25 +0100

    screenshots

--- cypress/support/pageObjects/automation-test-store/Autostore_Haircare_PO.js
+++ cypress/support/pageObjects/automation-test-store/Autostore_Haircare_PO.js
@@ -3,9 +3,14 @@
 class Autostore_Haircare_PO {
   addHairCareProductsToBasket() {
     globalThis.data.productName.forEach((product) => {
-      cy.addProductToBasket(product);
+      cy.addProductToBasket(product).then(() => {
+        //With this command I can debug in the browser the code
+        // debugger;
+      });
     });
     cy.get(".dropdown-toggle > .fa").click();
+    // in this case I will use debugger as a Promise
+    // .debug();
   }
 }
 
