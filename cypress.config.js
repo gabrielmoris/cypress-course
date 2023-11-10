edb18fbe772aabd1150d6f13cc95173ebb164f19
gabriel <gabrielcmoris@gmail.com>
2023-11-10 16:29:47 +0100

    different config files

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,22 @@
 const { defineConfig } = require("cypress");
+const fs = require("fs-extra");
+const path = require("path");
+
+function getConfigurationByFile(file) {
+  const pathToConfigFile = path.resolve("cypress", "config", `${file}.json`);
+
+  return fs.readJson(pathToConfigFile);
+}
 
 module.exports = defineConfig({
   projectId: "mao5un",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      // accept a configFile value or use development by default
+      const file = config.env.configFile || "development";
+
+      return getConfigurationByFile(file);
     },
     specPattern: "cypress/e2e/**/*.{js,jsx,tsx,feature}",
     exludeSpecPattern: "cypress/e2e/other/*.{js,jsx,tsx,feature}",
